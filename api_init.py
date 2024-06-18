a98de14bd91b7c92f608973598447b3efb8ee478
Dodant <ohho0506@gmail.com>
2024-06-18 10:39:31 +0900

    Refactor api_init.py and add to .gitignore

--- api_init.py
+++ api_init.py
@@ -1,12 +1,8 @@
 import os
+import config
 
-userID, hostIP, port = "compu", "220.117.189.130", 2322
-address = f"{userID}@{hostIP}"
-path = f"/home/{userID}/Downloads/tmp2/image-outpainting_2"
-cmd = "/compuworks/anaconda3"
-
-os.system(f"ssh {address} -p {port} \
-    \"cd {path} && \
-    source {cmd}/etc/profile.d/conda.sh && \
-    conda activate msd1_2 && \
-    python server.py\"")
+os.system(f"ssh {config.address} -p {config.port} \
+    \"cd {config.path} && \
+    source {config.conda_path}/etc/profile.d/conda.sh && \
+    conda activate {config.conda_env} && \
+    python {config.script}\"")
