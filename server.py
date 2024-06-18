c75f30202edad10541f355a8bb0081320a9ea51b
Dodant <ohho0506@gmail.com>
2024-06-18 11:01:49 +0900

    Refactor server.py

--- server.py
+++ server.py
@@ -1,18 +1,24 @@
-import os, time, uuid
+import os
+import os.path as pth
+import time
+import uuid
 from io import BytesIO
 from PIL import Image
-import warnings, logging
+import warnings
+import logging
 from logging.handlers import RotatingFileHandler
 
 from flask import Flask, request, jsonify, send_file
 from werkzeug.utils import secure_filename
+
+import config
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
 app.config['DOWNLOAD_FOLDER'] = 'downloads'
 app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
 
-log_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'app.log')
+log_file_path = pth.join(pth.dirname(pth.abspath(__file__)), 'app.log')
 handler = RotatingFileHandler('app.log', maxBytes=10000, backupCount=1)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
@@ -63,43 +69,44 @@ def handle_exception(e):
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
+def save_uploaded_file(file):
+    filename = secure_filename(file.filename)
+    filepath = pth.join(app.config['UPLOAD_FOLDER'], filename)
+    file.save(filepath)
+    return filepath, filename
+
+def execute_remote_commands(filepath, filename, method):
+    base_filename = ''.join(filename.split('.')[:-1])
+    remote_filepath = pth.join(config.path, 'req_img', filename)
+    output_filename = f"{base_filename}_{method}.jpg"
+    output_filepath = pth.join(config.path, 'req_img', output_filename)
+
+    scp_upload_cmd = f"scp -P {config.port} ./{filepath} {config.address}:{remote_filepath}"
+    curl_cmd = f"ssh {config.address} -p {config.port} \"curl -X POST -F 'image=@{remote_filepath}' -F 'method={method}' http://127.0.0.1:5004/process_image --output {output_filepath}\""
+    scp_download_cmd = f"scp -P {config.port} {config.address}:{output_filepath} ./{app.config['DOWNLOAD_FOLDER']}"
+    cleanup_cmd = f"ssh {config.address} -p {config.port} \"rm -r {config.path}/req_img/*\""
+
+    for cmd in [scp_upload_cmd, curl_cmd, scp_download_cmd, cleanup_cmd]:
+        os.system(cmd)
+
+    return output_filename
+
 @app.route('/prx', methods=['POST'])
 def prx_image():
     if 'image' not in request.files or 'method' not in request.form:
         return jsonify({'error': 'No image or method provided'}), 400
 
-    print("Headers:\n", request.headers)
-    print("Body:\n", request.get_data(as_text=True))
-    print("Form Data:\n", request.form)
-    print("Form method:\n", request.form.get('method'))
-    
-    if 'method' not in request.form:
-        return jsonify(error='No method part'), 400
-        
-    if 'image' not in request.files:
-        return jsonify(error='No image part'), 400
-    
     file = request.files['image']
     if file.filename == '':
         return jsonify(error='No selected file'), 400
     
     if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(filepath)
-
-        userID, hostIP, port = "compu", "220.117.189.130", 2322
-        address = f"{userID}@{hostIP}"
-        path = f"/home/{userID}/Downloads/tmp2/image-outpainting_2"
+        filepath, filename = save_uploaded_file(file)
         method = request.form.get('method')
+        output_filename = execute_remote_commands(filepath, filename, method)
+        download_path = pth.join(app.config['DOWNLOAD_FOLDER'], output_filename)
 
-        os.system(f"scp -P {port} ./{filepath} {address}:{path}/req_img/")
-        os.system(f"ssh {address} -p {port} \"curl -X POST -F 'image=@{path}/req_img/{filename}' -F 'method={method}' http://127.0.0.1:5004/process_image --output {path}/req_img/{''.join(filename.split('.')[:-1])}_{method}.jpg\"")
-        os.system(f"scp -P {port} {address}:{path}/req_img/{''.join(filename.split('.')[:-1])}_{method}.jpg ./{app.config['DOWNLOAD_FOLDER']}")
-        # os.system(f"ssh {address} -p {port} \"rm -r {path}/req_img/*\"")
-        
-        download_path = os.path.join(app.config['DOWNLOAD_FOLDER'], f"{''.join(filename.split('.')[:-1])}_{method}.jpg")
-        if os.path.exists(download_path):
+        if pth.exists(download_path):
             with open(download_path, 'rb') as file: 
                 img_data = file.read()
             return send_file(BytesIO(img_data), mimetype='image/jpeg')
@@ -109,8 +116,8 @@ def prx_image():
     return jsonify(error='Invalid file type'), 400
 
 if __name__ == '__main__':
-    if not os.path.exists(app.config['UPLOAD_FOLDER']): os.makedirs(app.config['UPLOAD_FOLDER'])
-    if not os.path.exists(app.config['DOWNLOAD_FOLDER']): os.makedirs(app.config['DOWNLOAD_FOLDER'])
+    if not pth.exists(app.config['UPLOAD_FOLDER']): os.makedirs(app.config['UPLOAD_FOLDER'])
+    if not pth.exists(app.config['DOWNLOAD_FOLDER']): os.makedirs(app.config['DOWNLOAD_FOLDER'])
     
-    hostIP = '0.0.0.0'
-    app.run(host=hostIP, port=5004, debug=True)
+    hostIP, port = '0.0.0.0', 5005
+    app.run(host=hostIP, port=port, debug=True)
